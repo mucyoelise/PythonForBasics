8c31c3c3873bfd74f13f6bd1df81485050ff64d7
mucyoelise <mucyo.bryant@gmail.com>
2024-10-24 05:45:54 +0200

    New commits

--- sells_machine.py
+++ sells_machine.py
@@ -1,3 +1,10 @@
+# Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins 
+# in these denominations: 25 cents, 10 cents, and 5 cents. Implement a program that prompts the 
+# user to insert a coin, one at a time, each time informing the user of the amount due. 
+# Once the user has inputted at least 50 cents, output how many cents in change the user is owed. 
+# Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+
+
 reserved_coin = [25,10,5]
 tot_coin = 0
 tot_required = 50
